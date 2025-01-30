893227cbdd78ae00e05eaf230fa48ffa91585e0b
Corbo <ppyrgiel@wp.pl>
2025-01-30 14:50:55 +0100

    created donation model

--- charity_app/models.py
+++ charity_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 INSTITUTION_CHOICES = (
@@ -17,3 +18,16 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.IntegerField(choices=INSTITUTION_CHOICES, default=0)
     categories = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateTimeField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,null=True, default=None)
