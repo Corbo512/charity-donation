edef2a24265ef855ab59c6b491ed5e2aa5999793
Corbo <ppyrgiel@wp.pl>
2025-01-30 14:25:12 +0100

    created institution model

--- charity_app/models.py
+++ charity_app/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+INSTITUTION_CHOICES = (
+    (0, "fundacja"),
+    (1, "organizacja pozarządowa"),
+    (2, "zbiórka lokalna")
+)
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.IntegerField(choices=INSTITUTION_CHOICES, default=0)
+    categories = models.ManyToManyField(Category)
