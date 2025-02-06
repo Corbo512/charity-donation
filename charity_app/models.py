2e3ed06e1b1cf9a15ea5c6f0e88135449f4976e8
Corbo <ppyrgiel@wp.pl>
2025-02-06 12:01:09 +0100

    moved user manager to models

--- charity_app/models.py
+++ charity_app/models.py
@@ -1,7 +1,66 @@
-from django.contrib.auth.models import AbstractUser
-from charity_app.utils import UserManager
 from django.db import models
+from django.apps import apps
+from django.contrib import auth
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager
 
+
+class UserManager(UserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Adres email jest wymagany')
+        email = self.normalize_email(email)
+        GlobalUserModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        email = GlobalUserModel.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
+
+    def with_perm(
+            self, perm, is_active=True, include_superusers=True, backend=None, obj=None
+    ):
+        if backend is None:
+            backends = auth._get_backends(return_tuples=True)
+            if len(backends) == 1:
+                backend, _ = backends[0]
+            else:
+                raise ValueError(
+                    "You have multiple authentication backends configured and "
+                    "therefore must provide the `backend` argument."
+                )
+        elif not isinstance(backend, str):
+            raise TypeError(
+                "backend must be a dotted import path string (got %r)." % backend
+            )
+        else:
+            backend = auth.load_backend(backend)
+        if hasattr(backend, "with_perm"):
+            return backend.with_perm(
+                perm,
+                is_active=is_active,
+                include_superusers=include_superusers,
+                obj=obj,
+            )
+        return self.none()
 
 class User(AbstractUser):
     username = None
