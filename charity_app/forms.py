a99984bacef93251affd610e296377326db45a46
Corbo <ppyrgiel@wp.pl>
2025-02-11 13:25:58 +0100

    register form complete

--- charity_app/forms.py
+++ charity_app/forms.py
@@ -1,24 +1,24 @@
 from django import forms
 from .models import User
-from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, label="Hasło")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Powtórz hasło")
+
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
 
     def clean(self):
         cleaned_data = super().clean()
-        password = self.cleaned_data.get("password")
-        password2 = self.cleaned_data.get("password2")
-        if password and password2 and password != password2:
-            raise forms.ValidationError("Hasła nie są identyczne.")
-        return cleaned_data
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+
+        if password and password2 and password != password2:
+            self.add_error('password2', 'Hasła nie są takie same')
+        if User.objects.filter(email=email).exists():
+            self.add_error('email', 'Konto o takim adresie email już istnieje')
+        return cleaned_data
