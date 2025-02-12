153248229c0a1c03d400e8a2abdfcb088c85ee99
Corbo <ppyrgiel@wp.pl>
2025-02-12 15:33:26 +0100

    load categories in form

--- charity_app/views.py
+++ charity_app/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LogoutView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 from django.views.generic.edit import CreateView
 from .forms import UserRegisterForm, UserLoginForm
-from .models import Donation, Institution
+from .models import Donation, Institution, Category
 
 
 class HomeView(TemplateView):
@@ -33,7 +34,12 @@ class UserRegisterView(CreateView):
 class UserLoginView(FormView):
     template_name = 'login.html'
     form_class = UserLoginForm
-    success_url = reverse_lazy('home')
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+        return reverse_lazy('home')
 
     def form_valid(self, form):
         email = form.cleaned_data.get('email')
@@ -50,15 +56,19 @@ class UserLoginView(FormView):
         else:
             return redirect('register')
 
-
-class AddDonationView(TemplateView):
+class AddDonationView(LoginRequiredMixin, TemplateView):
     template_name = 'form.html'
 
-class DonationConfirmationView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['categories'] = Category.objects.all()
+        return context
+
+class DonationConfirmationView(LoginRequiredMixin, TemplateView):
     template_name = 'form-confirmation.html'
 
 class UserLogoutView(LogoutView):
     next_page = 'home'
 
-class UserProfileView(TemplateView):
+class UserProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'user-profile.html'
